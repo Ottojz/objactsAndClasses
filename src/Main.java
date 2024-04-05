990575c06398dec03fcd24cecb0e3014f4d69267
Вадим <ottojz@yandex.ru>
2024-04-05 04:33:56 +0300

    Commit of homework branch, methods of objects lesson

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,25 @@ public class Main {
         Author rayBradbury = new Author("Ray", "Bradbury");
         Author lewisCarroll = new Author("Lewis", "Carroll");
 
-        System.out.println(lewisCarroll.getName() + " " + lewisCarroll.getSurname());
-        System.out.println(rayBradbury.getName() + " " + rayBradbury.getSurname());
+        System.out.println(lewisCarroll);
+        System.out.println(rayBradbury);
         System.out.println();
 
         Book fahrenheit451 = new Book("Fahrenheit 451", rayBradbury, (short) 1953);
         Book aliceInWonderland = new Book("Alice's Adventures in Wonderland", lewisCarroll, (short) 1865);
 
-        System.out.println(aliceInWonderland.getName() + " // " + aliceInWonderland.getAuthor() + " // " + aliceInWonderland.getYear());
-        System.out.println(fahrenheit451.getName() + " // " + fahrenheit451.getAuthor() + " // " + fahrenheit451.getYear());
+        System.out.println(aliceInWonderland);
+        System.out.println(fahrenheit451);
         System.out.println();
 
         fahrenheit451.setYear((short) 1952);
 
-        System.out.println(aliceInWonderland.getName() + " // " + aliceInWonderland.getAuthor() + " // " + aliceInWonderland.getYear());
-        System.out.println(fahrenheit451.getName() + " // " + fahrenheit451.getAuthor() + " // " + fahrenheit451.getYear());
+        System.out.println(aliceInWonderland);
+        System.out.println(fahrenheit451);
+        System.out.println();
+
+        System.out.println (rayBradbury.equals (rayBradbury));
+        System.out.println (fahrenheit451.equals (fahrenheit451));
 
     }
 }
